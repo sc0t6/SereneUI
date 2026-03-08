75f88e9ef3da9e164b4afaac9a191a7f232e4d62
scot <riftedkris@gmail.com>
2026-03-08 23:32:57 +0200

    Update NormalUI.lua

--- SereneHub/NormalUI.lua
+++ SereneHub/NormalUI.lua
@@ -1,28 +1,19 @@
 local SereneUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/sc0t6/SereneUI/main/serenelib.lua"))()
 
-local Window = SereneUI:CreateWindow("SereneUI Demo")
+local Window = SereneUI:CreateWindow("Serene Hub")
 
 local MainTab = Window:CreateTab("Main")
-local Section = MainTab:CreateSection("Player")
 
-Section:Button("Print Hello","blue",function()
-	print("Hello!")
+MainTab:Button("Print Hello", function()
+print("Hello")
 end)
 
-Section:Toggle("Fly",function(state)
-	print("Fly:",state)
+MainTab:Toggle("Toggle Test", false, function(state)
+print(state)
 end)
 
-Section:Slider("Speed",0,100,function(value)
-	print(value)
-end)
-
-Section:Textbox("Enter Name",function(text)
-	print(text)
-end)
-
-Section:Dropdown("Select Fruit",{"Apple","Banana","Orange"},function(choice)
-	print(choice)
+Main:Slider("Slider Test",1,10,function(v)
+print(v)
 end)
 
 SereneUI:Notify("SereneUI","Loaded Successfully!",5)
