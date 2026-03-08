9a2c4d17f22c9d20f9e09d3ce23dcfaac37a9d0a
scot <riftedkris@gmail.com>
2026-03-08 23:11:54 +0200

    Create NormalUI.lua

---
+++ SereneHub/NormalUI.lua
@@ -0,0 +1,7 @@
+local SereneUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/sc0t6/SereneUI/refs/heads/main/serenelib.lua"))()
+
+SereneUI:Label("Serene UI has Loaded.")
+
+SereneUI:Button("Printing","blue",function()
+    print("Test Button")
+end)
